bc53d86cf86f8de689755dd5ad33d9cdc02b521d
Henrikki Helander <henrikki_helander@hotmail.com>
2020-12-09 23:43:09 +0200

    added updateMember api call to frontend

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -22,10 +22,10 @@ export class MemberEditComponent implements OnInit {
     }
   }
 
-  constructor(private accountService: AccountService, private memberService: MembersService, 
+  constructor(private accountService: AccountService, private memberService: MembersService,
     private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
-   }
+  }
 
   ngOnInit(): void {
     this.loadMember();
@@ -39,8 +39,9 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
       this.toastr.success("Profile updated succesfully");
       this.editForm.reset(this.member);
+    })
   }
 }
